a6806f13e6df99db088ffed0edb464f1c68cbcb4
Omulosi <mulongojohnpaul@gmail.com>
2019-07-26 13:24:52 +0300

    created the home page URL conf

--- crmapp/urls.py
+++ crmapp/urls.py
@@ -15,7 +15,21 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from marketing.views import HomePage
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    # Marketing pages
+    url(r'^$', HomePage.as_view(), name="home")
+
+    # Subscriber related URLs
+
+    # Admin URL
+
+    # Login/Logout URLs
+
+    # Account related URLs
+
+    # Contact related URLs
+
+    # Communication related URLs
 ]
