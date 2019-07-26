37b2cf16d2db250b54ad8610afefcd7a9f1cc42a
Omulosi <mulongojohnpaul@gmail.com>
2019-07-26 21:56:24 +0300

    add django-heroku to settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,13 +1,12 @@
 from .base import *
 from .base import env
+import dj_database_url
 
 DEBUG = False
 
-DATABASES['default'] = env.db('DATABASE_URL')
+DATABASES['default'] = dj_database_url.config()
 DATABASES['default']['ATOMIC_REQUESTS'] = True
 
 ALLOWED_HOSTS = ['*']
 
 SECRET_KEY = env('SECRET_KEY')
-
-django_heroku.settings(locals())
