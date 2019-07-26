1826fd7c3d157760dd95030034b04d96604ca61d
Omulosi <mulongojohnpaul@gmail.com>
2019-07-26 22:21:50 +0300

    update deploy settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import environ
-import django_heroku
 
 ROOT_DIR = (
     environ.Path(__file__) - 3
--- config/wsgi.py
+++ config/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crmapp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 
 application = Cling(get_wsgi_application())
