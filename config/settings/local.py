a89f38d8b0baa6a45f95e164b33c56723afe60ee
Omulosi <mulongojohnpaul@gmail.com>
2019-07-30 17:44:19 +0300

    installed and configured Stripe

--- config/settings/local.py
+++ config/settings/local.py
@@ -3,10 +3,17 @@ Settings for local development environment
 """
 
 from .base import *
+from .base import env
 
 DEBUG = True
 
 SECRET_KEY = env("SECRET_KEY", default="nfekhrf84jfjfi9rjr%$343")
+
+# Stripe settings
+STRIPE_SECRET_KEY = env('STRIPE_SECRET_KEY')
+STRIPE_PUBLISHABLE_KEY = env('STRIPE_PUBLISHABLE_KEY')
+SUBSCRIPTION_PRICE = 1500
+
 
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
 
