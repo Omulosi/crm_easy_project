39752faeff24f9238e547c13c19539923f4bb3dd
Omulosi <mulongojohnpaul@gmail.com>
2019-08-02 09:17:49 +0300

    added new account URL

--- crm_easy/accounts/urls.py
+++ crm_easy/accounts/urls.py
@@ -6,5 +6,6 @@ app_name = 'accounts'
 urlpatterns = [
     url(r'^account/list/$', views.AccountList.as_view(), name='account_list'),
     url(r'^account/(?P<uuid>[\w-]+)/$', views.account_detail, name='account_detail'),
+    url(r'^account/new/$', views.account_cru, name='account_new'),
 ]
 
