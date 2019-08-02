15d5e1d402a46e18c2668e7263852b1b1bb1a443
Omulosi <mulongojohnpaul@gmail.com>
2019-08-02 18:26:00 +0300

    created new account view

--- crm_easy/contacts/views.py
+++ crm_easy/contacts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, HttpResponseForbidden
+from django.core.urlresolvers import reverse
 
 from .models import Contact
-
+from .forms import ContactForm
 
 @login_required
 def contact_detail(request, uuid):
@@ -12,3 +14,35 @@ def contact_detail(request, uuid):
     return render(request, 
                   'contacts/contacts_detail.html',
                   {'contact': contact})
+
+
+@login_required()
+def contact_cru(request):
+
+    if request.POST:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            # make sure the user owns the account
+            account = form.cleaned_data['account']
+            if account.owner != request.user:
+                return HttpResponseForbidden()
+            # save the data
+            contact = form.save(commit=False)
+            contact.owner = request.user
+            contact.save()
+            # return the user to the account detail view
+            reverse_url = reverse(
+                'accounts:account_detail',
+                args=(account.uuid,)
+            )
+            return HttpResponseRedirect(reverse_url)
+    else:
+        form = ContactForm()
+
+    context = {
+        'form': form,
+    }
+
+    template = 'contacts/contact_cru.html'
+
+    return render(request, template, context)
