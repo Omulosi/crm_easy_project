2166396ade31d72ee7ff12961fe3da38c7bc09af
Omulosi <mulongojohnpaul@gmail.com>
2019-08-02 19:07:55 +0300

    modified contact view to allow for editing

--- crm_easy/contacts/views.py
+++ crm_easy/contacts/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, HttpResponseForbidden
 from django.core.urlresolvers import reverse
+from diango.shortcuts import get_object_or_404
 
 from .models import Contact
 from .forms import ContactForm
+from ..accounts.models import Account
+
 
 @login_required
 def contact_detail(request, uuid):
@@ -17,10 +20,17 @@ def contact_detail(request, uuid):
 
 
 @login_required()
-def contact_cru(request):
-
+def contact_cru(request, uuid=None, account=None):
+    
+    if uuid:
+        contact = get_object_or_404(Contact, uuid=uuid)
+        if contact.owner != request.user:
+            return HttpResponseForbidden()
+    else:
+        contact = Contact(owner=request.user)
+    
     if request.POST:
-        form = ContactForm(request.POST)
+        form = ContactForm(request.POSTi, instance=contact)
         if form.is_valid():
             # make sure the user owns the account
             account = form.cleaned_data['account']
@@ -36,11 +46,19 @@ def contact_cru(request):
                 args=(account.uuid,)
             )
             return HttpResponseRedirect(reverse_url)
+        else:
+            # if the form isn't valid, still fetch the account so it can be passed to the template
+            account = form.cleaned_data['account']
     else:
         form = ContactForm()
 
+    if request.GET.get('account', ''):
+        account = Account.objects.get(id=request.GET.get('account', ''))
+
     context = {
         'form': form,
+        'contact': contact,
+        'account': account
     }
 
     template = 'contacts/contact_cru.html'
